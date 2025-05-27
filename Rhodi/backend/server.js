7391c3cdcf33c1bf8f5118e8d0bbe8d6e1f5e0e6
lecongminhthao <lecongminhthao2004@gmail.com>
2025-05-28 00:03:44 +0700

    CRUD Color với CRUD nhân viên

--- Rhodi/backend/server.js
+++ Rhodi/backend/server.js
@@ -1,20 +1,42 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
-
+const mongoose = require("mongoose");
 // Cấu hình biến môi trường
+const authRoutes = require("./admin/routers/Auth");
+const ColorRouter = require("./admin/routers/ColorRouter");
+const authMiddleware = require("./admin/middleware/authMiddleware");
+const employeeRoutes = require("./admin/routers/EmployessRouter");
+
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("bạn đã chạy thành công");
-});
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.originalUrl}`);
+  next();
+});
+app.use("/api", authRoutes);
+app.use("/admin", authMiddleware(["Admin"]), ColorRouter);
+app.use("/admin", authMiddleware(["Admin"]), employeeRoutes);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/rhodi-datn")
+  .then(() => console.log(" kết nối thành công!"))
+  .catch((err) => {
+    console.error(" thất bại rồi:", err);
+    process.exit(1);
+  });
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server đang chạy tại http://localhost:${port}`);
 });
